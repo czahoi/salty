41cdb9a37ec9b3c3b5f86de1cb494fff7dbb8e38
lemonsn <38351007+lemonsn@users.noreply.github.com>
2019-10-25 14:05:25 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -221,7 +221,7 @@ autoSwap()
 		mkdir /www
 	fi
 	swapFile='/www/swap'
-	dd if=/dev/zero of=$swapFile bs=2G count=1
+	dd if=/dev/zero of=$swapFile bs=8192 count=256
 	mkswap -f $swapFile
     swapon $swapFile
     echo "$swapFile    swap    swap    defaults    0 0" >> /etc/fstab
